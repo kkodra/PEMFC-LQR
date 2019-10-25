c3cb432908b9ab4ddacd41e758bf3f60c2ce9314
kkodra <ksk2035@icloud.com>
2019-10-25 19:09:56 -0400

    Algorithm to evaluate exact SP decomposition added

---
+++ eval_L_H.m
@@ -0,0 +1,58 @@
+% Function evaluates matrices L and H using:
+%           - Recursive method
+%           - Eigenvectors method
+% Syntax: [L,H] = eval_L_H(A1,A2,A3,A4,epsilon,method)
+% User can select 'recursive' or 'eigenvector' for method.
+
+function [L,H] = eval_L_H(A1,A2,A3,A4,epsilon,method)
+
+if strcmp(method, 'recursive') == 1
+    Li = (A4)\A3;
+    for j = 1:1000
+        D1 = A4 +epsilon*Li*A2;
+        D2 = -epsilon*(A1 - A2*Li);
+        Qi = A3 +epsilon*Li*A2*Li;
+        Li_1 = lyap(D1,D2,-Qi);
+        Hi_1 = lyap(D2,D1,-A2);
+        Lnew = Li_1;
+        Hnew = Hi_1;
+    end
+    L = Lnew;
+    H = Hnew;
+elseif strcmp(method, 'eigenvector') == 1
+    R = [-epsilon*A1 epsilon*A2; A3 -A4];
+    [V,~] = eig(R);
+    
+    M = V;
+    i = 1;
+    Mnew = zeros(length(M));
+    while i  <= length(R)
+        if isreal(M(:,i))== 0
+            Mnew(:,i) = real(M(:,i));
+            Mnew(:,i+1) = imag(M(:,i));
+            if i == length(R)
+                break
+            end
+            i = (i+1) + 1;
+        else
+            Mnew(:,i) = M(:,i);
+            i = i + 1;
+        end
+    end
+
+    M1 = Mnew(:,1:length(A1));
+    M11 = M1(1:length(A1),:);
+    M21 = M1(length(A1)+ 1:end,:);
+    L = M21/M11;
+
+    % Find L
+    % Test_L = A4*L - epsilon*L*A1 - A3 + L*epsilon*A2*L   % Test is good
+
+    Asyl = -epsilon*(A1-A2*L);
+    Bsyl = A4 + epsilon*L*A2;
+    Csyl = -A2;
+
+    H = lyapkr(Asyl,Bsyl,Csyl);
+end
+end
+
