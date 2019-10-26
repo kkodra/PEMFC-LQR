778962443d119388a6e10e45031a7319cf5e9ee2
kkodra <ksk2035@icloud.com>
2019-10-26 13:33:19 -0400

    Main file to run all the scripts created

---
+++ main.m
@@ -0,0 +1,14 @@
+% Main script to run algorithms
+% (Work in progress)
+
+clear
+clc
+
+% Import model
+[sys_aug, sys_SP, sys_eig] = PEMFC_FPS_Model;
+
+% Create a SP model using developed algorithm
+% TODO
+
+% Controller design
+% TODO
