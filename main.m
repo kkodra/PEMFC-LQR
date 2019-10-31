4922ebfc77d189353df477f538fff02fb1cecbbf
kkodra <ksk2035@icloud.com>
2019-10-31 00:06:31 -0400

    Changes to main.m

--- main.m
+++ main.m
@@ -26,8 +26,8 @@ end
 %% Controller design
 % Controllability matrices 
 SP_cont = ctrb(A,B);
-slow_cont = ctrb(slow_sys.As, slow_sys.Bs);
-fast_cont = ctrb(slow_sys.Af, slow_sys.Bf);
+slow_cont = ctrb(slow_sys.A, slow_sys.B);
+fast_cont = ctrb(fast_sys.A, fast_sys.B);
 
 % LQR for original SP model
 if rank(SP_cont) < rank(A)
@@ -48,5 +48,10 @@ r = ones(size(t));
 % LQR controller for overall augmented system
 Q_aug = sys_aug.C'*sys_aug.C;
 R_aug = 1;
-
 [K_aug, sys_FB_aug, y_aug, x_aug] = LQR_control(Q_aug, R_aug, sys_aug, r, t)
+
+% LQR controller for slow subsystem
+Q_slow = slow_sys.C'*slow_sys.C;
+R_slow = 1;
+
+[K_slow, sys_FB_slow, y_slow, x_slow] = LQR_control(Q_slow, R_slow, sys_slow, r, t)
