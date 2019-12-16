c05fd004c6f3b7193c3d9eadbbaf3f18645672de
kkodra <ksk2035@icloud.com>
2019-12-15 21:06:36 -0500

    Added TODO list

--- decouple_sys.m
+++ decouple_sys.m
@@ -1,5 +1,8 @@
 function [slow_sys, fast_sys, LH_test, L, H] = decouple_sys(A,B,C,dim,epsilon)
 
+% TODO: Create loop. Run len(dim) times and create subsystems
+% NEEDS modification to agree with algorithm in paper
+
 % From bottom to top [1 4]
 A1 = A(1:dim,1:dim);
 A2 = A(1:dim,dim+1:end);
